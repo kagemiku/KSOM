08d30cfa1b389efd5861977499a458e04e3c0afb
KAGE <kagemiku@gmail.com>
2015-11-04 09:46:17 +0900

    Add core files.

---
+++ KSOM.h
@@ -0,0 +1,152 @@
+#pragma once
+
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include "Node.h"
+
+
+namespace {
+	const double MAX_DISTANCE = std::numeric_limits<double>::max();
+};
+
+
+template <typename T>
+class KSOM {
+private:
+	const Node<T>* src_;
+	const int length_;
+	const int dimension_;
+
+	Node<T>** map_;
+	const int rows_;
+	const int cols_;
+
+	const double alpha0_;
+	const double sigma0_; 
+	const int maxIterate_;
+	int time_;
+	
+private:
+	double calcAlpha(int time);
+	double calcSigma(int time);
+	double calcH(double distance, int time);
+	double calcDistance(int x1, int y1, int x2, int y2);
+	double calcDistance(const Node<T>& node1, const Node<T>& node2); 
+ 
+public:
+	KSOM(Node<T>* const src, int length, Node<T>** map, int rows, int cols, int maxIterate, double alpha0, double sigma0);
+	~KSOM(void);
+
+	bool calcIterate();
+	int time() const; 
+};
+
+
+template <typename T>
+KSOM<T>::KSOM(Node<T>* const src, int length, Node<T>** map, int rows, int cols, int maxIterate, double alpha0, double sigma0)
+	:src_(src)
+	,length_(length)
+	,dimension_(src[0].size())
+	,map_(map)
+	,rows_(rows)
+	,cols_(cols)
+	,alpha0_(alpha0)
+	,sigma0_(sigma0)
+	,maxIterate_(maxIterate)
+	,time_(0)
+{
+}
+
+
+template <typename T>
+KSOM<T>::~KSOM(void)
+{
+}
+
+ 
+template <typename T>
+double KSOM<T>::calcAlpha(int time)
+{
+	return alpha0_ * exp(-static_cast<double>(time)/static_cast<double>(maxIterate_));
+}
+
+
+template <typename T>
+double KSOM<T>::calcSigma(int time)
+{
+	return sigma0_ * exp(-static_cast<double>(time)/static_cast<double>(maxIterate_));
+}
+
+
+template <typename T>
+double KSOM<T>::calcH(double distance, int time)
+{
+	return exp(-pow(distance, 2.0)/(2*pow(calcSigma(time), 2.0)));
+}
+
+
+template <typename T>
+double KSOM<T>::calcDistance(int x1, int y1, int x2, int y2)
+{
+	return sqrt(pow(x1 - x2, 2.0) + pow(y1 - y2, 2.0));
+}
+
+
+template <typename T>
+double KSOM<T>::calcDistance(const Node<T>& node1, const Node<T>& node2)
+{
+	double dis = 0.0;
+	for ( int i = 0; i < dimension_; i++ ) {
+		dis += pow(node1[i] - node2[i], 2.0);
+	}
+
+	return sqrt(dis);
+}
+
+
+template <typename T>
+bool KSOM<T>::calcIterate()
+{
+	if ( time_ >= maxIterate_ ) {
+		return false;
+	}
+
+    const int idx = time_;
+	const Node<T>& refNode = src_[idx];
+	double minDis = MAX_DISTANCE;
+	int minDisRow = 0, minDisCol = 0;
+	for ( int r = 0; r < rows_; r++ ) {
+		for ( int c = 0; c < cols_; c++ ) {
+			double dis = calcDistance(refNode, map_[r][c]);
+			if ( dis < minDis ) {
+				minDis = dis;
+				minDisRow = r;
+				minDisCol = c;
+			}
+		}
+	}
+ 
+	const double alpha = calcAlpha(time_);
+	for ( int r = 0; r < rows_; r++ ) {
+		for ( int c = 0; c < cols_; c++ ) {
+			const double dis = calcDistance(r, c, minDisRow, minDisCol);
+			const double h = calcH(dis, time_);
+
+			for ( int i = 0; i < dimension_; i++ ) {
+				map_[r][c][i] += static_cast<T>(h*alpha*(refNode[i] - map_[r][c][i]));
+			}
+		}
+	} 
+
+	++time_;
+    
+	return true;
+}
+
+
+template <typename T>
+int KSOM<T>::time() const
+{
+	return time_;
+}
---
+++ Node.h
@@ -0,0 +1,255 @@
+#pragma once
+
+#include <string>
+
+
+template <typename T>
+class Node {
+private:
+	T* elems_;
+	int size_;
+ 
+private:
+	void copyMember(const Node<T>& rhs);
+
+public:
+	Node(int size=1);
+	Node(const Node<T>& rhs);
+	~Node(void);
+	Node<T> operator+() const;
+	Node<T> operator-() const;
+	Node<T> operator+(const Node<T>& rhs) const;
+	Node<T> operator+(T val) const;
+	Node<T> operator-(const Node<T>& rhs) const;
+	Node<T> operator-(T val) const;
+	Node<T>& operator=(const Node<T>& rhs);
+	Node<T>& operator+=(const Node<T>& rhs);
+	Node<T>& operator+=(T val);
+	Node<T>& operator-=(const Node<T>& rhs);
+	Node<T>& operator-=(T val);
+	T& operator[](int idx) const;
+	void setElem(T elem, int idx);
+	T elem(int idx) const;
+	int size(void) const;
+}; 
+
+
+template <typename T>
+void Node<T>::copyMember(const Node<T>& rhs)
+{
+	for ( int i = 0; i < size_; i++ ) {
+		this->elems_[i] = rhs.elems_[i];
+	}
+}
+
+
+template <typename T>
+Node<T>::Node(int size)
+	:size_(size) 
+{ 
+	elems_ = new T[size_];
+	memset(elems_, 0, sizeof(elems_));
+} 
+
+
+template <typename T>
+Node<T>::Node(const Node<T>& rhs) 
+	:size_(rhs.size_)
+{
+	elems_ = new T[size_];
+	copyMember(rhs);
+}
+
+
+template <typename T>
+Node<T>::~Node(void) 
+{ 
+	delete[] elems_;
+}
+
+
+template <typename T>
+Node<T> Node<T>::operator+() const
+{
+	Node<T> node(*this);
+
+	return node;
+}
+
+
+template <typename T>
+Node<T> Node<T>::operator-() const 
+{
+	Node<T> node(size_);
+	for ( int i = 0; i < size_; i++ ) {
+		node.elems_[i] = -this->elems_[i];
+	}
+
+	return node;
+}
+
+
+template <typename T>
+Node<T> Node<T>::operator+(const Node& rhs) const
+{
+	if ( size_ != rhs.size_ ) {
+		throw std::string("different size");
+	}
+
+	Node<T> node(size_);
+	for ( int i = 0; i < size_; i++ ) {
+		node.elems_[i] = this->elems_[i] + rhs.elems_[i];
+	}
+
+	return node;
+}
+
+
+template <typename T>
+Node<T> Node<T>::operator+(T val) const
+{
+	Node<T> node(size_);
+	for ( int i = 0; i < size_; i++ ) {
+		node.elems_[i] = this->elems_[i] + val;
+	}
+
+	return node;
+}
+
+
+template <typename T>
+Node<T> Node<T>::operator-(const Node& rhs) const
+{
+	if ( size_ != rhs.size_ ) {
+		throw std::string("different size");
+	}
+
+	Node<T> node(size_);
+	for ( int i = 0; i < size_; i++ ) {
+		node.elems_[i] = this->elems_[i] - rhs.elems_[i];
+	}
+
+	return node;
+}
+
+
+template <typename T>
+Node<T> Node<T>::operator-(T val) const
+{
+	Node<T> node(size_);
+	for ( int i = 0; i < size_; i++ ) {
+		node.elems_[i] = this->elems_[i] - val;
+	}
+
+	return node;
+}
+
+
+template <typename T>
+Node<T>& Node<T>::operator=(const Node& rhs)
+{
+	if ( this == &rhs ) {
+		return *this;
+	}
+
+	size_ = rhs.size_;
+
+	T* tmp = elems_;
+	elems_ = new T[size_];
+	delete[] tmp;
+	copyMember(rhs);
+
+	return *this;
+}
+
+
+template <typename T>
+Node<T>& Node<T>::operator+=(const Node& rhs) 
+{
+	if ( size_ != rhs.size_ ) {
+		throw std::string("different size");
+	}
+
+	for ( int i = 0; i < size_; i++ ) {
+		elems_[i] += rhs.elems_[i];
+	}
+
+	return *this;
+}
+
+
+template <typename T>
+Node<T>& Node<T>::operator+=(T val)
+{
+	for ( int i = 0; i < size_; i++ ) {
+		elems_[i] += val;
+	}
+
+	return *this;
+}
+
+
+template <typename T>
+Node<T>& Node<T>::operator-=(const Node& rhs)
+{
+	if ( size_ != rhs.size_ ) {
+		throw std::string("different size");
+	}
+
+	for ( int i = 0; i < size_; i++ ) {
+		elems_[i] -= rhs.elems_[i];
+	}
+
+	return *this;
+}
+
+
+template <typename T>
+Node<T>& Node<T>::operator-=(T val)
+{
+	for ( int i = 0; i < size_; i++ ) {
+		elems_[i] -= val;
+	}
+
+	return *this;
+}
+
+
+template <typename T>
+T& Node<T>::operator[](int idx) const
+{
+	if ( idx < 0 || idx >= size_ ) {
+		throw std::string("out of range.");
+	}
+
+	return elems_[idx];
+}
+
+
+template <typename T>
+void Node<T>::setElem(T elem, int idx)
+{
+	if ( idx >= size_ ) {
+		throw std::string("out of range.");
+	}
+
+	elems_[idx] = elem;
+}
+
+
+template <typename T>
+T Node<T>::elem(int idx) const
+{
+	if ( idx >= size_ ) {
+		throw std::string("out of range.");
+	}
+
+	return elems_[idx];
+}
+
+
+template <typename T>
+int Node<T>::size(void) const
+{
+	return size_;	
+}
