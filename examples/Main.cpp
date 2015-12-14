af3af348102bbf8b75772cf800184a7f94486c81
kagemiku <kagemiku@gmail.com>
2015-12-14 14:26:28 +0900

    Add process of finding best alpha0 value.

--- examples/Main.cpp
+++ examples/Main.cpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <random>
+#include <chrono>
 #include "../KSOM.h"
 #include "../Node.h"
 #include "../Evaluater.h"
@@ -14,6 +15,7 @@ using namespace std;
 namespace {
     constexpr auto RGB_MIN = 0;
     constexpr auto RGB_MAX = 255;
+    constexpr auto EPS = 10e-6;
 }
 
 
@@ -47,19 +49,38 @@ int main()
     }
     
 
-    // create instance of KSOM and compute
+    // finding best alpha0
     constexpr auto maxIterate = 10000;
-    constexpr auto alpha0 = 0.1;
+    constexpr auto maxAlpha0 = 1.0f;
+    constexpr auto diffAlpha0 = 0.05f;
     constexpr auto sigma0 = 20.0;
-    auto colorSOM = make_unique<kg::KSOM<int>>(src, map, maxIterate, alpha0, sigma0);
-    while ( colorSOM->computeOnes() ) {
-        //cout << colorSOM->time() << endl;
+    constexpr auto repeat = 10;
+    
+    for ( auto alpha0 = diffAlpha0; alpha0 <= maxAlpha0 + EPS; alpha0 += diffAlpha0 ) {
+        auto totalTime = 0;
+        auto totalEvaluateValue = 0.0f;
+        for ( auto n = 0; n < repeat; n++ ) {
+            // create instance of KSOM and compute
+            auto colorSOM = make_unique<kg::KSOM<int>>(src, map, maxIterate, alpha0, sigma0);
+            auto start = chrono::system_clock::now();
+            colorSOM->compute();
+            auto end = chrono::system_clock::now();
+            auto diff = end - start;
+            totalTime += chrono::duration_cast<chrono::milliseconds>(diff).count();
+
+            // evaluate map created by KSOM
+            auto evaluater = make_unique<kg::Evaluater<int>>(colorSOM->map());
+            auto evaluateValue = evaluater->evaluateMap();
+            cout << n << ": " << evaluateValue << endl;
+            totalEvaluateValue += evaluateValue;
+        }
+
+        auto meanTime = static_cast<double>(totalTime)/static_cast<double>(repeat);
+        auto meanEvaluateValue = static_cast<double>(totalEvaluateValue)/static_cast<double>(repeat);
+        cout << "alpha0: " << alpha0 << " ..." << meanTime << "[ms]" << endl;
+        cout << "evaluate value: " << meanEvaluateValue << endl; 
+        cout << endl;
     }
-
-
-    // evaluate map created by KSOM
-    auto evaluater = make_unique<kg::Evaluater<int>>(colorSOM->map());
-    cout << "evaluate value: " << evaluater->evaluateMap() << endl; 
 
     return 0;
 }
