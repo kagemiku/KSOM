c360b4b233fb1a35a8453a6a80e3f4ae26086d7d
kagemiku <kagemiku@gmail.com>
2015-12-15 11:55:37 +0900

    Add process of finding best sigma0.

--- examples/Main.cpp
+++ examples/Main.cpp
@@ -55,6 +55,43 @@ auto evaluateAlpha0(const vector<kg::Node<T>>& source, const vector<vector<kg::N
     }
 }
 
+
+template <typename T>
+auto evaluateSigma0(const vector<kg::Node<T>>& source, const vector<vector<kg::Node<T>>>& map, int size) -> void
+{
+    constexpr auto maxIterate = 10000;
+    constexpr auto alpha0 = 0.8f;
+    constexpr auto diffSigma0 = 5.0f;
+    constexpr auto repeat = 1;
+    
+    for ( auto sigma0 = static_cast<double>(size); sigma0 >= 0.0f - EPS; sigma0 -= diffSigma0 ) {
+        auto totalTime = 0;
+        auto totalEvaluateValue = 0.0f;
+        for ( auto n = 0; n < repeat; n++ ) {
+            // create instance of KSOM and compute
+            auto colorSOM = make_unique<kg::KSOM<T>>(source, map, maxIterate, alpha0, sigma0);
+            auto start = chrono::system_clock::now();
+            colorSOM->compute();
+            auto end = chrono::system_clock::now();
+            auto diff = end - start;
+            totalTime += chrono::duration_cast<chrono::milliseconds>(diff).count();
+
+            // evaluate map created by KSOM
+            auto evaluater = make_unique<kg::Evaluater<T>>(colorSOM->map());
+            auto evaluateValue = evaluater->evaluateMap();
+            cout << n << ": " << evaluateValue << endl;
+            totalEvaluateValue += evaluateValue;
+        }
+
+        auto meanTime = static_cast<double>(totalTime)/static_cast<double>(repeat);
+        auto meanEvaluateValue = static_cast<double>(totalEvaluateValue)/static_cast<double>(repeat);
+        cout << "sigma0: " << sigma0 << " ..." << meanTime << "[ms]" << endl;
+        cout << "evaluate value: " << meanEvaluateValue << endl; 
+        cout << endl;
+    }
+}
+
+
 int main()
 { 
     // create array of input vector
@@ -86,7 +123,10 @@ int main()
     
 
     // finding best alpha0
-    evaluateAlpha0(src, map);
+    //evaluateAlpha0(src, map);
+
+    // finding best sigma0
+    evaluateSigma0(src, map, rows);
 
     return 0;
 }
