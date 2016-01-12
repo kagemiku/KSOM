514a974936c10779d124f59f004b131de70ecf57
kage-mac <kagemiku@gmail.com>
2016-01-12 09:57:56 +0900

    Change main.cpp to evaluate map another method.

--- examples/main.cpp
+++ examples/main.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <random>
 #include <chrono>
+#include <algorithm>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include "../sources/ksom.hpp"
@@ -27,8 +28,59 @@ namespace {
     constexpr auto RED_IDX          = 2;
     constexpr auto GREEN_IDX        = 1;
     constexpr auto BLUE_IDX         = 0;
-    constexpr auto VIEWER_WIDTH     = 800;
-    constexpr auto VIEWER_HEIGHT    = 800;
+
+    constexpr auto length           = 1024;
+    constexpr auto dimension        = 3;
+    constexpr auto rows             = 32;
+    constexpr auto cols             = 32;
+    constexpr auto matWidth         = 800;
+    constexpr auto matHeight        = 800;
+}
+
+
+enum class SavingType : int {
+    ORIGINAL    = 1,
+    RESULT      = 2,
+};
+
+
+template <typename T>
+void createMapMat(const vector<vector<kg::Node<T>>>& map, int rows, int cols, cv::Mat& image)
+{
+    const auto blockWidth   = matWidth/cols;
+    const auto blockHeight  = matHeight/rows;
+
+    for ( auto r = 0; r < rows; r++ ) {
+        for ( auto c = 0; c < cols; c++ ) {
+            cv::Point rectPos1(c * blockWidth, r * blockHeight);
+            cv::Point rectPos2((c + 1) * blockWidth, (r + 1) * blockHeight);
+            cv::Scalar rectColor(map[r][c][BLUE_IDX], map[r][c][GREEN_IDX], map[r][c][RED_IDX]);
+            cv::rectangle(image, rectPos1, rectPos2, rectColor, -1);
+        }
+    }
+}
+
+
+auto fileNameOfMat(SavingType type, const string& suffixNumStr) -> string
+{
+    string fileName("result/");
+    switch ( type ) {
+        case SavingType::ORIGINAL:
+            fileName += string("original");
+            break;
+
+        case SavingType::RESULT:
+            fileName += string("result");
+            break;
+    }
+    fileName += string("_");
+
+    string suffix(suffixNumStr);
+    replace(begin(suffix), end(suffix), '.', '_');
+    fileName += suffix;
+    fileName += string(".png");
+
+    return fileName;
 }
 
 
@@ -38,25 +90,34 @@ auto evaluateAlpha0(const vector<kg::Node<T>>& source, const kg::Matrix<kg::Node
     const auto maxIterate       = 500*source.size();
     constexpr auto maxAlpha0    = 1.0;
     constexpr auto diffAlpha0   = 0.1;
-    constexpr auto sigma0       = 5.0;
-    constexpr auto repeat       = 10;
+    constexpr auto sigma0       = 16;
+    constexpr auto repeat       = 1;
+
 
     for ( auto alpha0 = diffAlpha0; alpha0 <= maxAlpha0 + EPS; alpha0 += diffAlpha0 ) {
         auto totalTime              = 0;
         auto totalEvaluationValue   = 0.0;
         for ( auto n = 0; n < repeat; n++ ) {
             // create instance of KSOM and compute
-            auto colorSOM   = make_unique<kg::KSOM<T>>(source, map, maxIterate, alpha0, sigma0);
+            auto colorSOM       = make_unique<kg::KSOM<T>>(source, map, maxIterate, alpha0, sigma0);
+            auto originalMat    = cv::Mat(cv::Size(matWidth, matHeight), CV_8UC3, cv::Scalar::all(0));
+            createMapMat(colorSOM->map(), rows, cols, originalMat);
+            cv::imwrite(fileNameOfMat(SavingType::ORIGINAL, to_string(alpha0)), originalMat);
+
             auto start      = chrono::system_clock::now();
             colorSOM->compute();
             auto end        = chrono::system_clock::now();
             auto diff       = end - start;
             totalTime += chrono::duration_cast<chrono::milliseconds>(diff).count();
 
+            auto resultMat  = originalMat.clone();
+            createMapMat(colorSOM->map(), rows, cols, resultMat);
+            cv::imwrite(fileNameOfMat(SavingType::RESULT, to_string(alpha0)), resultMat);
+
             // evaluate map created by KSOM
             auto resultMap          = colorSOM->map();
             auto evaluationValue    = kg::Evaluator<T>::evaluateMap(source, resultMap);
-            cout << n << ": " << evaluationValue << endl;
+            // cout << n << ": " << evaluationValue << endl;
             totalEvaluationValue += evaluationValue;
         }
 
@@ -105,28 +166,9 @@ auto evaluateSigma0(const vector<kg::Node<T>>& source, const kg::Matrix<kg::Node
 }
 
 
-template <typename T>
-void createMapMat(const vector<vector<kg::Node<T>>>& map, int rows, int cols, cv::Mat& image)
-{
-    const auto blockWidth= VIEWER_WIDTH/cols;
-    const auto blockHeight  = VIEWER_HEIGHT/rows;
-
-    for ( auto r = 0; r < rows; r++ ) {
-        for ( auto c = 0; c < cols; c++ ) {
-            cv::Point rectPos1(c * blockWidth, r * blockHeight);
-            cv::Point rectPos2((c + 1) * blockWidth, (r + 1) * blockHeight);
-            cv::Scalar rectColor(map[r][c][BLUE_IDX], map[r][c][GREEN_IDX], map[r][c][RED_IDX]);
-            cv::rectangle(image, rectPos1, rectPos2, rectColor, -1);
-        }
-    }
-}
-
-
 int main()
 {
     // create array of input vector
-    constexpr auto length       = 100;
-    constexpr auto dimension    = 3;
     vector<kg::Node<int>> src(length, kg::Node<int>(dimension));
 
     random_device rnd;
@@ -139,7 +181,6 @@ int main()
     }
 
     // create matrix of model vector
-    constexpr auto rows = 10, cols = 10;
     kg::Matrix<kg::Node<int>> map(rows, vector<kg::Node<int>>(cols));
 
     uniform_int_distribution<> randIdx(0, length - 1);
