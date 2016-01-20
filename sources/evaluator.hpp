3734ba20e0fd15c2261701d8c51f32d0848c7799
kage-mac <kagemiku@gmail.com>
2016-01-20 09:33:12 +0900

    Format evaluator.hpp

--- sources/evaluator.hpp
+++ sources/evaluator.hpp
@@ -1,6 +1,7 @@
 #ifndef KG_EVALUATOR_H
 #define KG_EVALUATOR_H
 
+
 #include <string>
 #include <vector>
 #include <cmath>
@@ -18,9 +19,12 @@ using Matrix = std::vector<std::vector<T>>;
 template <typename T>
 class Evaluator {
 private:
-    static auto isValidArgs(const std::vector<kg::Node<T>>& source, const Matrix<kg::Node<T>>& map) -> bool;
-    static auto calcDistance(const Node<T>& node1, const Node<T>& node2) -> double;
-    static auto findNearestNode(const kg::Node<T>& node, const Matrix<kg::Node<T>>& map) -> kg::Node<T>;
+    static auto isValidArgs(const std::vector<kg::Node<T>>& source,
+            const Matrix<kg::Node<T>>& map) -> bool;
+    static auto calcDistance(const Node<T>& node1,
+            const Node<T>& node2) -> double;
+    static auto findNearestNode(const kg::Node<T>& node,
+            const Matrix<kg::Node<T>>& map) -> kg::Node<T>;
 
 public:
     Evaluator() = delete;
@@ -28,7 +32,8 @@ public:
     ~Evaluator();
     auto operator=(const Evaluator<T>& rhs) -> Evaluator<T> = delete;
 
-    static auto evaluateMap(const std::vector<kg::Node<T>>& source, const Matrix<kg::Node<T>>& map) throw (std::string) -> double;
+    static auto evaluateMap(const std::vector<kg::Node<T>>& source,
+            const Matrix<kg::Node<T>>& map) throw (std::string) -> double;
 };
 
 
@@ -39,7 +44,8 @@ Evaluator<T>::~Evaluator()
 
 
 template <typename T>
-auto Evaluator<T>::isValidArgs(const std::vector<kg::Node<T>>& source, const Matrix<kg::Node<T>>& map) -> bool
+auto Evaluator<T>::isValidArgs(const std::vector<kg::Node<T>>& source,
+        const Matrix<kg::Node<T>>& map) -> bool
 {
     if ( source[0].size() != map[0][0].size() ) {
         return false;
@@ -67,7 +73,8 @@ auto Evaluator<T>::isValidArgs(const std::vector<kg::Node<T>>& source, const Mat
 
 
 template <typename T>
-auto Evaluator<T>::calcDistance(const Node<T>& node1, const Node<T>& node2) -> double
+auto Evaluator<T>::calcDistance(const Node<T>& node1,
+        const Node<T>& node2) -> double
 {
     const auto dimension = node1.size();
     auto dis = 0.0;
@@ -115,7 +122,8 @@ auto Evaluator<T>::findNearestNode(const kg::Node<T>& node, const Matrix<kg::Nod
 
 
 template <typename T>
-auto Evaluator<T>::evaluateMap(const std::vector<kg::Node<T>>& source, const Matrix<kg::Node<T>>& map) throw (std::string) -> double
+auto Evaluator<T>::evaluateMap(const std::vector<kg::Node<T>>& source,
+        const Matrix<kg::Node<T>>& map) throw (std::string) -> double
 {
     if ( !isValidArgs(source, map) ) {
         throw std::string("invalid arguments");
